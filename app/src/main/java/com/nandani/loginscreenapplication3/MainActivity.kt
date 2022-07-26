1694d403a787713168aa34a90ba60aa10d52a42b
nandanikoundal <109542801+nandanikoundal@users.noreply.github.com>
2022-07-26 13:37:14 +0530

    change layout
    change layout relative layout to linear layout

--- app/src/main/java/com/nandani/loginscreenapplication3/MainActivity.kt
+++ app/src/main/java/com/nandani/loginscreenapplication3/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.nandani.loginscreenapplication3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var email:EditText
+    lateinit var Password: EditText
+    lateinit var forgetpassword:TextView
+    lateinit var Login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        email = findViewById(R.id.email)
+        Password = findViewById(R.id.Password)
+        forgetpassword = findViewById(R.id.forgetpassword)
+        Login = findViewById(R.id.Login)
+
+        Login.setOnClickListener {
+            System.out.println("Button is Clicked")
+        }
+
+            if (email.text.isNullOrEmpty()) {
+
+                email.error = resources.getString(R.string.please_enter_Email)
+            }
+                 else if (Password.text.isNullOrEmpty()) {
+                     Password.error=resources.getString(R.string.please_enter_password)
+
+                }
+                else if(Password.text.length<6){
+                    Password.error=resources.getString(R.string.short_pass)
+            }
+                else {
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.login_successfully),
+                    Toast.LENGTH_LONG).show()
+
+
+            }
+        }
     }
-}
+
+
+
+
